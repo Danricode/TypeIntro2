747c1a24aa6bd27a40a1179719d2c357a54a5f0e
Daniel Silva <danielaudrasilva@gmail.com>
2022-05-23 12:19:00 +0200

    Typescript test using concatenated variables

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+const name = "Daniel";
+const age = 26;
+let message = name+" "+age;
+export default function App(): JSX.Element {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{message}</Text>
+      <Text></Text>
       <StatusBar style="auto" />
     </View>
   );
